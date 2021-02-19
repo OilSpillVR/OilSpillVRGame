f21dbb6cbe9242701d6a696f2c9cc9f687565096
Paffa <pate009@gmail.com>
2021-02-19 11:14:16 +0200

    Added button testing

---
+++ Assets/Scripts/ButtonActionController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonActionController : MonoBehaviour
+{
+    private Animator _anim;
+    [SerializeField] private GameObject _hoverIndicator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+      
+
+    public void ButtonDownAnimation()
+    {
+        Debug.Log("Button pressed");
+        _anim.SetTrigger("ButtonDown");
+    }
+
+    public void ShowHoverIndicator()
+    {
+        _hoverIndicator.SetActive(true);
+    }
+    public void HideHoverIndicator()
+    {
+        _hoverIndicator.SetActive(false);
+    }
+}
