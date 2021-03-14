6dfa93015669e7493130c6a34253b426dd93fba4
Francisco Carrasco <francagir@hotmail.com>
2021-03-14 17:24:08 +0200

    Added some lobby scripts
    Creating different "server" with differnet (5 characters, numbers and letters)

---
+++ Assets/Scripts/UILobby.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace OilSpillVR
+{
+    public class UILobby : MonoBehaviour
+    {
+        public static UILobby instance;
+        [SerializeField] private InputField joinMatchInput;
+        [SerializeField] private Button hostButton;
+        [SerializeField] private Button joinButton;
+        [SerializeField] private Canvas lobbyCanvas;
+
+        private void Start()
+        {
+            instance = this;
+        }
+
+        public void Host()
+        {
+            joinMatchInput.interactable = false;
+            hostButton.interactable = false;
+            joinButton.interactable = false;
+
+            Player.localPlayer.HostGame();
+        }
+
+        public void Join()
+        {
+            joinMatchInput.interactable = false;
+            hostButton.interactable = false;
+            joinButton.interactable = false;
+        }
+
+        public void HostSuccess(bool success)
+        {
+            if (success)
+            {
+                lobbyCanvas.enabled = true;
+            }
+            else
+            {
+                joinMatchInput.interactable = true;
+                hostButton.interactable = true;
+                joinButton.interactable = true;
+            }
+        }
+
+        public void JoinSuccess(bool success)
+        {
+            if (success)
+            {
+            }
+            else
+            {
+                joinMatchInput.interactable = true;
+                hostButton.interactable = true;
+                joinButton.interactable = true;
+            }
+        }
+    }
+}
