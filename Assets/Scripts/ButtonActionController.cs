3849697d062b21d758dac34a367d3f74c3b4b131
Paffa <pate009@gmail.com>
2021-03-15 14:27:02 +0200

    Player interactions. Lever test. Snap point. Teleportation and UI interactions.

--- Assets/Scripts/ButtonActionController.cs
+++ Assets/Scripts/ButtonActionController.cs
@@ -5,27 +5,32 @@ using UnityEngine;
 public class ButtonActionController : MonoBehaviour
 {
     private Animator _anim;
-    [SerializeField] private GameObject _hoverIndicator;
+    [SerializeField] private GameObject _button;
+    private Renderer objRenderer;
+    private Color startColor;
 
     // Start is called before the first frame update
     void Start()
     {
         _anim = GetComponent<Animator>();
+
+        objRenderer = _button.GetComponent<Renderer>();
+        startColor = objRenderer.material.color;
     }
       
 
     public void ButtonDownAnimation()
     {
-        Debug.Log("Button pressed");
+        Debug.Log("Activate Button pressed");
         _anim.SetTrigger("ButtonDown");
     }
 
     public void ShowHoverIndicator()
     {
-        _hoverIndicator.SetActive(true);
+        objRenderer.material.color = Color.white;
     }
     public void HideHoverIndicator()
     {
-        _hoverIndicator.SetActive(false);
+        objRenderer.material.color = startColor;
     }
 }
